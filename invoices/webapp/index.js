a339e342174640a040682f324263401dc1ef81a3
Daniel de León <dhdezleon@gmail.com>
2022-10-09 18:24:25 +0000

    Hola Mundo desde SAP.m.TEXT

--- invoices/webapp/index.js
+++ invoices/webapp/index.js
@@ -1,8 +1,13 @@
-sap.ui.define([], 
-    
-    
-    function(){
+sap.ui.define([ 
+   "sap/m/Text"
+],  
+/**
+ * 
+ * @param {typeof sap.m.Text} Text 
+ */
+    function(Text){
 
         alert("UI5 es correcto");
+        new Text({text:"Hola Mundo desde Content"}).placeAt("content");
 
 });
