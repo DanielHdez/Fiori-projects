441461dd4acda83a56844065c7efe682176f3bfc
Daniel de León <dhdezleon@gmail.com>
2022-10-14 16:58:09 +0000

    Hola Mundo desde Vista

--- invoices/webapp/index.js
+++ invoices/webapp/index.js
@@ -1,13 +1,21 @@
-sap.ui.define([ 
-   "sap/m/Text"
-],  
-/**
- * 
- * @param {typeof sap.m.Text} Text 
- */
-    function(Text){
+sap.ui.define([
+    "sap/ui/core/mvc/XMLView"
+],
+    /**
+     * 
+     * @param {typeof sap.ui.core.mvc.XMLView} PEPE
+     */
+    function (PEPE) {
+     "use strict";
+        
+        /**Para que el js me de mas información */
+        /**alert("UI5 es correcto");
+         new Text({text:"Hola Mundo desde Content"}).placeAt("content");*/
+         PEPE.create({
+            viewName: "dhl.invoices.view.App"
+        }).then(function (oView) {
+            oView.placeAt("content");
 
-       /**alert("UI5 es correcto"); */ 
-        new Text({text:"Hola Mundo desde Content"}).placeAt("content");
+        });
+    });
 
-});
