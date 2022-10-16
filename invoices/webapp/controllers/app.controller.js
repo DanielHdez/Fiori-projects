ed8a0e29b6477f03d702d44ffc135a079f59d200
Daniel de León <dhdezleon@gmail.com>
2022-10-16 15:37:23 +0000

    Usando traducciones i18n

--- invoices/webapp/controllers/app.controller.js
+++ invoices/webapp/controllers/app.controller.js
@@ -3,22 +3,27 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "dhl/invoices/Models/Model"
+    "dhl/invoices/Models/Model",
+    "sap/ui/model/resource/ResourceModel"
 ], 
 
 /**
 * @param {typeof sap.ui.core.mvc.Controller} Controllers
 * @param {typeof sap.m.MessageToast} MessageToasts
+* @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel
 */
-function(Controllers,MessageToasts,Model) {
+function(Controllers,MessageToasts,Model,ResourceModel,Text) {
     'use strict';
-
     return Controllers.extend("dhl.invoices.controllers.app",{
 
          onInit: function(){
-
+            // Modelo de los datos
              this.getView().setModel(Model.CreateRecipient());
-         
+            
+            //Modelo de traducciones 
+
+            var i18nModel= new ResourceModel({bundleName : "dhl.invoices.i18n.i18n"})
+            this.getView().setModel(i18nModel,"i18n");
           /** *  let oData ={
                 recipient: { name: "Mundo D" }
             };
@@ -26,7 +31,11 @@ function(Controllers,MessageToasts,Model) {
             this.getView().setModel(oModel);*/
         },
         onShowHola: function( ){
-            MessageToasts.show("Hola desde mensaje de sap.m desde Controlador");
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            var srecipient =  this.getView().getModel().getProperty("/recipient/name");
+            var mensaje = oBundle.getText("Holamsg",[srecipient]);
+          // MessageToasts.show("Hola desde mensaje de sap.m desde Controlador");
+            MessageToasts.show(mensaje);
             
            // alert("Hola Mundo desde Controlador");
         }
