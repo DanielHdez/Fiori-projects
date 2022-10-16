9d344fa4ba9afbee7df7e711c8cb8739f37ffada
Daniel de León <dhdezleon@gmail.com>
2022-10-16 08:28:46 +0000

    Texto dinamico desde JSONModel

--- invoices/webapp/controllers/app.controller.js
+++ invoices/webapp/controllers/app.controller.js
@@ -2,18 +2,27 @@
 
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
 ], 
 
 /**
 * @param {typeof sap.ui.core.mvc.Controller} Controllers
 * @param {typeof sap.m.MessageToast} MessageToasts
+* @param {typeof sap.ui/model.json.JSONModel} JSONModel
 */
-function(Controllers,MessageToasts) {
+function(Controllers,MessageToasts,JSONModel) {
     'use strict';
 
     return Controllers.extend("dhl.invoices.controllers.app",{
-        
+
+         onInit: function( ){
+            let oData ={
+                recipient: { name: "Mundo D" }
+            };
+            const oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+        },
         onShowHola: function( ){
             MessageToasts.show("Hola desde mensaje de sap.m desde Controlador");
             
