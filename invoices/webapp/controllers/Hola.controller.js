f1ee99b81094ffc76cd355b98a9f0ad20f7d2054
Daniel de León <dhdezleon@gmail.com>
2022-11-01 14:18:40 +0000

    Dialogos

--- invoices/webapp/controllers/Hola.controller.js
+++ invoices/webapp/controllers/Hola.controller.js
@@ -2,31 +2,54 @@
 
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], 
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
+],
 
-/**
-* @param {typeof sap.ui.core.mvc.Controller} Controllers
-* @param {typeof sap.m.MessageToast} MessageToasts
-*/
-function(Controllers,MessageToasts) {
-    'use strict';
-    return Controllers.extend("dhl.invoices.controllers.Hola",{
+    /**
+    * @param {typeof sap.ui.core.mvc.Controller} Controllers
+    * @param {typeof sap.m.MessageToast} MessageToasts
+    * @param {typeof sap.ui.core.Fragment"} Fragment
+    */
+    function (Controllers, MessageToasts, Fragment) {
+        'use strict';
+        return Controllers.extend("dhl.invoices.controllers.Hola", {
 
-         onInit: function(){
-          
-        },
-        onShowHola: function( ){
-            
-            var oBundle = this.getView().getModel("i18n").getResourceBundle();
-            var srecipient =  this.getView().getModel().getProperty("/recipient/name");
-            var mensaje = oBundle.getText("Holamsg",[srecipient]);
-          // MessageToasts.show("Hola desde mensaje de sap.m desde Controlador");
-            MessageToasts.show(mensaje);
-            
-           // alert("Hola Mundo desde Controlador");
-        }
+            onInit: function () {
+
+            },
+            onShowHola: function () {
+
+                var oBundle = this.getView().getModel("i18n").getResourceBundle();
+                var srecipient = this.getView().getModel().getProperty("/recipient/name");
+                var mensaje = oBundle.getText("Holamsg", [srecipient]);
+                // MessageToasts.show("Hola desde mensaje de sap.m desde Controlador");
+                MessageToasts.show(mensaje);
+
+                // alert("Hola Mundo desde Controlador");
+            },
+            onOpenDialog: function () {
+                const oView = this.getView();
+            if (!this.byId("HolaDialogo")){
+                Fragment.load({
+                    id: oView.getId(),
+                    name: "dhl.invoices.view.HolaDialogo",
+                    controller: this
+                }).then(function (oDialog) {
+                    oView.addDependent(oDialog);
+                    oDialog.open();
+                });
+            } else {
+                this.byId("HolaDialogo").open();
+            }
+
+            },
+
+            onCloseDialogo: function(){
+                this.byId("HolaDialogo").close();
+
+            }
+
+        });
 
     });
-    
-});
