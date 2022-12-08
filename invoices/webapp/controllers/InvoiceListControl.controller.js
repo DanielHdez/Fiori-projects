af7c5aa27483a5c300660a25efe9a595fa045e94
Daniel <dhdezleon@gmail.com>
2022-12-08 14:28:08 +0000

    8.5. Filtros

--- invoices/webapp/controllers/InvoiceListControl.controller.js
+++ invoices/webapp/controllers/InvoiceListControl.controller.js
@@ -2,15 +2,20 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "../Models/InvoiceFormater"
+    "../Models/InvoiceFormater",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+
     
 ],
 
     /**
     * @param {typeof sap.ui.core.mvc.Controller} Controllers
     * @param {typeof sap.ui.model.json.JSONModel} JSONModel
+    * @param {typeof sap.ui.model.Filter} Filter
+    * @param {typeof sap.ui.model.FilterOperator} FilterOperator
     */
-    function (Controller, JSONModel,InvoiceFormater) {
+    function (Controller, JSONModel,InvoiceFormater, Filter, FilterOperator) {
         'use strict';
       
       return Controller.extend("dhl.invoices.controllers.InvoiceListControl",{
@@ -22,6 +27,18 @@ sap.ui.define([
 
           });
           this.getView().setModel(objViewModel, "currency");     
+        },
+        onFilterInvoices: function(oEvent){
+          const ArrayFilter = [];
+          const sQuery= oEvent.getParameter("query");
+          if  (sQuery) {
+            //ArrayFilter.push(new Filter("ProductName", "EQ", sQuery));
+            ArrayFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+          };
+
+          const oList = this.getView().byId("lista");
+          const oBinding = oList.getBinding("items");
+          oBinding.filter(ArrayFilter);
         }
 
       });
